4162890b55756a403397807974fe1add975bbe23
rutik nimkarde <Rutik Nimkarde>
2025-07-22 22:03:59 +0530

    NonDupliInArrayLeetcode

---
+++ NonDupliInArray.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class NonDupliInArray
+{
+    public static int ssr(int arr[])
+    {
+        HashSet<Integer>set=new HashSet<>();
+
+        for(int i=0;i<arr.length;i++)
+        {
+            set.add(arr[i]);
+        }
+        return set.size();
+    }
+    public static void main(String[] args) {
+        int arr[]={0,0,1,1,1,2,2,3,3,4};
+        System.out.print(ssr(arr));
+    }
+}
